b34eac2d92c290cb15231d624dea375ee5e36737
Nguyen Huu Long <longnh.0601@gmail.com>
2022-05-26 11:15:08 +0700

    add chart

--- app/src/main/java/com/nt118/joliecafeadmin/adapter/BestSellerAdapter.kt
+++ app/src/main/java/com/nt118/joliecafeadmin/adapter/BestSellerAdapter.kt
@@ -1,16 +1,25 @@
 package com.nt118.joliecafeadmin.adapter
 
-import android.app.Activity
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
+import com.nt118.joliecafeadmin.databinding.ItemBestSellerBinding
 
 
-class BestSellerAdapter(
-) {
+class BestSellerAdapter: RecyclerView.Adapter<BestSellerAdapter.ViewHolder>() {
+    class ViewHolder(binding: ItemBestSellerBinding) : RecyclerView.ViewHolder(binding.root) {
+        val binding = binding
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemBestSellerBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount() = 10
 }
