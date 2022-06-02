d61b89367617aab1582fd676f85fa30128d6690b
NguyenKhue09 <19521712@gm.uit.edu.vn>
2022-06-02 20:30:35 +0700

    notification ui

--- app/src/main/java/com/nt118/joliecafeadmin/ui/fragments/revenue/RevenueFragment.kt
+++ app/src/main/java/com/nt118/joliecafeadmin/ui/fragments/revenue/RevenueFragment.kt
@@ -14,6 +14,7 @@ import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.XAxis
@@ -50,7 +51,16 @@ class RevenueFragment : Fragment() {
 
         println(revenueViewModel.adminToken)
 
+        buttonClickListener()
+
         return binding.root
+    }
+
+    private fun buttonClickListener() {
+        binding.btnNotifications.setOnClickListener {
+            val action = RevenueFragmentDirections.actionNavigationRevenueToNotificationsFragment()
+            findNavController().navigate(action)
+        }
     }
 
     private fun initViews() {
